d80e9cd2526a6b39255e0da21341915c718ff3d2
Raymond Cepeda <33160305+rcepeda456@users.noreply.github.com>
2020-01-09 14:39:00 -0500

    Add files via upload

--- practice.js
+++ practice.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Car extends React.Component {
+class First extends React.Component{
   render() {
-    return <h2>I am a Car!</h2>;
+    return
+    <h1> Hello, This is my first React Component!</h1>;
   }
 }
-
-class Garage extends React.Component {
-  render() {
-    return (
-      <div>
-      <h1>Who lives in my Garage?</h1>
-      <Car />
-      </div>
-    );
-  }
-}
+ class Second extends React.Component{
+   render() {
+     return (
+     <div>
+     <h2> This is my Second Component!</h2>
+     </First>
+     </div>
+     );
+   }
+ }
